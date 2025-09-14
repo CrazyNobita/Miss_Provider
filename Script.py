469e6d641424a03c29af80467022b6a84a70f05b
˹ 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 ˼ <skr346902@gmail.com>
2025-09-14 10:34:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -217,7 +217,8 @@ Nᴀᴍᴇ : {}
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
     CAPTION = """<blockquote><a href="https://t.me/MissProviderBot">{file_name}</a></b>\n\n<blockquote>"""
- 
+
+
    MOVIE_UPDATE_NOTIFY_TXT = """
 </b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
 
