b189ebd62d2f395473aa7861866db80d66f05bf6
˹ 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 ˼ <skr346902@gmail.com>
2025-10-03 21:16:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -34,15 +34,10 @@ class script(object):
 🚀 ꜰᴏʟʟᴏᴡ ᴛʜᴇꜱᴇ ꜱᴛᴇᴘꜱ!
 </b>"""
 
-    ABOUT_TXT = """<b>╭────[ ᴍʏ ᴅᴇᴛᴀɪʟs ]────⍟
-├⍟ Mʏ Nᴀᴍᴇ : <a href=https://t.me/{}>{}</a>
-├⍟ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href={}>ᴏᴡɴᴇʀ</a> 
-├⍟ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a>
-├⍟ Lᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 𝟹</a> 
-├⍟ Dᴀᴛᴀʙᴀsᴇ : <a href='https://www.mongodb.com/'>ᴍᴏɴɢᴏ ᴅʙ</a> 
-├⍟ Bᴏᴛ Sᴇʀᴠᴇʀ : <a href='https://heroku.com/'>ʜᴇʀᴏᴋᴜ</a> 
-├⍟ Bᴜɪʟᴅ Sᴛᴀᴛᴜs : ᴠ1.4 [ ꜱᴛᴀʙʟᴇ ]
-╰───────────────⍟</b>"""
+    ABOUT_TXT = """<blockquote>CHAT GPT USES ‣</blockquote>
+<b>➻ ᴀɢᴀɪɴ ᴛᴀᴘ ᴛʜᴇ ɢᴘᴛ ʙᴜᴛᴛᴏɴ .
+<blockquote> GEMINI USES ‣</blockquote>
+<b>➻ ᴀɢᴀɪɴ ᴛᴀᴘ ᴛʜᴇ ɢᴇᴍɪɴɪ ʙᴜᴛᴛᴏɴ</b>"""
     RESTART_TXT = """
 <b>{} Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
 
