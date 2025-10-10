a093664b89838875cdbdabdbd9fa5b05b1b79a80
˹ 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 ˼ <skr346902@gmail.com>
2025-10-10 07:00:10 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -46,13 +46,9 @@ class Media(Document):
 @instance2.register
 class Media2(Document):
     file_id = fields.StrField(attribute="_id")
-    file_ref = fields.StrField(allow_none=False)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=False)
-    mime_type = fields.StrField(allow_none=False)
-    caption = fields.StrField(allow_none=False)
-
+    
     class Meta:
         indexes = ("$file_name",)
         collection_name = COLLECTION_NAME
