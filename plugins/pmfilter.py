c992f2d35df72431aff8a837064b031dfa91aef7
˹ 𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 ˼ <skr346902@gmail.com>
2025-11-10 19:56:00 +0530

    Fix photo input in edit_message_media call

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1560,7 +1560,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto('https://graph.org/file/99eebf5dbe8a134f548e0.jpg')
+            InputMediaPhoto('https://graph.org/file/762337ddbfefe4ef5fb96-0468bf018693d08e64.jpg')
         )
         await query.message.edit_text(
             text=script.DREAMXBOTZ_DONATION.format(query.from_user.mention, QR_CODE, OWNER_UPI_ID),
